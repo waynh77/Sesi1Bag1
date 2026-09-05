4daeaf7f325d60b4f5e48410962b4f340bca694f
Wahyu Nurhidayat <60255281+waynh77@users.noreply.github.com>
2026-09-05 16:29:41 +0700

    perubahan 1

--- Sesi1Bag1/Program.cs
+++ Sesi1Bag1/Program.cs
@@ -6,42 +6,42 @@ namespace Sesi1Bag1
     {
         static void Main()
         {
-            //Console.Write("Nama: ");
-            //string? nama = Console.ReadLine();
+            Console.Write("Nama: ");
+            string? nama = Console.ReadLine();
 
-            //Console.Write("Umur: ");
-            //int umur = int.Parse(Console.ReadLine() ?? "0");
+            Console.Write("Umur: ");
+            int umur = int.Parse(Console.ReadLine() ?? "0");
 
-            //Console.Write("Kota: ");
-            //string? kota = Console.ReadLine();
+            Console.Write("Kota: ");
+            string? kota = Console.ReadLine();
 
-            //Console.WriteLine($"Halo {nama} dari {kota}.");
-            //Console.WriteLine($"Tahun depan umur kamu {umur + 1} tahun.");
+            Console.WriteLine($"Halo {nama} dari {kota}.");
+            Console.WriteLine($"Tahun depan umur kamu {umur + 1} tahun.");
 
-            //Console.WriteLine("Halo " + nama + " dari " + kota + ".");
-            //Console.WriteLine("Tahun depan umur kamu " + (umur + 1) + " tahun.");
+            Console.WriteLine("Halo " + nama + " dari " + kota + ".");
+            Console.WriteLine("Tahun depan umur kamu " + (umur + 1) + " tahun.");
 
             //const double Phi = 3.14159;
 
             // Salah
             //Phi = 3.14;
 
-            Console.Write("Nama: ");
-            string nama = Console.ReadLine();
+            //Console.Write("Nama: ");
+            //string nama = Console.ReadLine();
 
-            Console.Write("Umur: ");
-            int umur = int.Parse(Console.ReadLine());
+            //Console.Write("Umur: ");
+            //int umur = int.Parse(Console.ReadLine());
 
-            Console.Write("Tinggi badan: ");
-            double tinggi = double.Parse(Console.ReadLine());
+            //Console.Write("Tinggi badan: ");
+            //double tinggi = double.Parse(Console.ReadLine());
 
-            Console.Write("Berat badan: ");
-            double berat = double.Parse(Console.ReadLine());
+            //Console.Write("Berat badan: ");
+            //double berat = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Nama: {nama}");
-            Console.WriteLine($"Umur: {umur} tahun");
-            Console.WriteLine($"Tinggi: {tinggi} cm");
-            Console.WriteLine($"Berat: {berat} kg");
+            //Console.WriteLine($"Nama: {nama}");
+            //Console.WriteLine($"Umur: {umur} tahun");
+            //Console.WriteLine($"Tinggi: {tinggi} cm");
+            //Console.WriteLine($"Berat: {berat} kg");
 
             //ini komen 1 baris
             Console.ReadLine();
